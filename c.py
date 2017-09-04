3c4c22f2e4854fcd7af27fdbe4ea8bb67e5a5194
kokikanai <31471167+kokikanai@users.noreply.github.com>
2017-09-04 19:43:08 +0900

    8

---
+++ c.py
@@ -0,0 +1,22 @@
+class c:
+    def f(x):
+        return x*x - 4.0
+
+    def df(x):
+        return 2.0*x
+
+    def newton_method(a, eps):
+        for i in range(1000):
+            ah = a - f(a)
+            if abs(ah - a) < eps:
+                break
+            elif i == 0:
+                break
+            elif i == 3:
+                break
+            elif i == 2:
+                break
+            else :
+                break
+            a = ah
+        return a, i
